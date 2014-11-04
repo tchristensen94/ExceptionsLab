f9ef0069bb64aca353e43f550d2b47a55ab081c1
rustedarrows <ch4nger@gmail.com>
2014-11-04 15:11:23 -0600

    Lab

---
+++ src/lab2/NameService.java
@@ -0,0 +1,62 @@
+package lab2;
+
+/**
+ * This class provides various services relating to name manipulation.
+ * No output should be performed here.
+ * 
+ * @author  Jim Lombardo, jlombardo@wctc.edu
+ * @version 1.00
+ */
+public class NameService {
+    private static final int FIRST_NAME_IDX = 0;
+    private static final int LAST_NAME_IDX = 1;
+    
+    /**
+     * Finds and returns the last name from within a full name. Caution: 
+     * No validation is performed.
+     * 
+     * @param fullName - a name containing a first name and a last name
+     * @return the last name
+     */
+    public String extractLastName(String fullName) {
+        if(!validateName(fullName)) {
+            throw new IllegalArgumentException("Name null or less than two characters");
+        }
+        String[] nameParts = fullName.split(" ");
+        return nameParts[LAST_NAME_IDX];
+    }
+    
+    /**
+     * Finds and returns the first name from within a full name. Caution: 
+     * No validation is performed.
+     * 
+     * @param fullName - a name containing a first name and a last name
+     * @return the first name
+     */
+    public String extractFirstName(String fullName) {
+        if(!validateName(fullName)) {
+            throw new IllegalArgumentException();
+        }
+        String[] nameParts = fullName.split(" ");
+        return nameParts[FIRST_NAME_IDX];
+    }
+
+    /**
+     * Gets the length of a name.
+     * 
+     * @param name - any full name or part of a name.
+     * @return the length of the name or part.
+     */
+    public int getNameLength(String name) {
+        return name.length();
+    }
+    
+    private boolean validateName(String name) {
+        if(name == null || name.length() < 2) {
+            return false;
+        }
+        String[] nameParts = name.split(" ");
+        return nameParts.length != 2;
+    }
+    
+}
